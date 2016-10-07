a48a256d8e1ec437dc2a1ebbc361f4224c7944b9
Sami Meharzi <smeharzi@rgnt2-67-243-dhcp.int.colorado.edu>
2016-10-07 14:00:40 -0600

    Finished assignment 5.

--- hw5/hw5.c
+++ hw5/hw5.c
@@ -1,30 +1,22 @@
 /*
  * Simple program that builds a House like object and puts them together
- * to form a cool pattern and allows orthogonal and perspective projections.
- * This was made using code from ex9.c and my Hw3
+ * to form a cool object. There is also a light that spins around allowing
+ * demonstrating how light affects the scene differently
+ * This was made using code from ex13.c and my Hw4
  */
 
-//Libraries and Shit
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <math.h>
-
-#define GL_GLEXT_PROTOTYPES
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
+#include "CSCIx229.h"
 
 /*  Global Variables  */
 int th=0;       // Azimuth of view angle
 int ph=0;       // Elevation of view angle
 int mode=0;     // Dimension (1-4)
-double dim=7.5;   // Dimension of orthogonal box
+double dim=10;   // Dimension of orthogonal box
 char* text[] = {"3D"};  // Dimension display text
 int fov=55;       //  Field of view (for perspective)		<---- Taken from ex9.c
 double asp=1;     //  Aspect ratio 				<---- Taken from ex9.c
+int light=1;
+int move=1;
 //double dim=5.0;   //  Size of world				<---- Taken from ex9.c
 
 int house1 = 1;	// Variables to turn on display for each of the objects
@@ -34,9 +26,19 @@ int house4 = 1;
 int house5 = 1;
 int house6 = 1;
 
-//  Macro for sin & cos in degrees			<---- Taken from ex9.c
-#define Cos(th) cos(th * 3.1415927/180)
-#define Sin(th) sin(th * 3.1415927/180)
+int one       =   1;  // Unit value         <---- All these variables taken from ex13.c
+int distance  =   5;  // Light Distance     
+int inc       =  10;  // Ball increment     
+int smooth    =   1;  // Smooth/Flat shading
+int local     =   0;  // Local Viewer Model
+int emission  =   0;  // Emission intensity (%)
+int ambient   =  30;  // Ambient intensity (%)
+int diffuse   = 100;  // Diffuse intensity (%)
+int specular  =   0;  // Specular intensity (%)
+int shininess =   0;  // Shininess (power of two)
+float shiny   =   1;  // Shininess (value)
+int zh        =  90;  // Light azimuth
+float ylight  =   0;  // Elevation of light
 
 /*
  *  Convenience routine to output raster text <---- Taken from ex6.c
@@ -57,22 +59,55 @@ void Print(const char* format , ...)
 		  glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
 }
 
-static void Project()
+
+static void Vertex(double th,double ph)
 {
-   //  Tell OpenGL we want to manipulate the projection matrix
-   glMatrixMode(GL_PROJECTION);
-   //  Undo previous transformations
-   glLoadIdentity();
-   //  Perspective transformation
-   if (mode)
-      gluPerspective(fov,asp,dim/10,10*dim);
-   //  Orthogonal projection
-   else
-      glOrtho(-asp*dim,+asp*dim, -dim,+dim, -dim,+dim);
-   //  Switch to manipulating the model matrix
-   glMatrixMode(GL_MODELVIEW);
-   //  Undo previous transformations
-   glLoadIdentity();
+   double x = Sin(th)*Cos(ph);
+   double y = Cos(th)*Cos(ph);
+   double z =         Sin(ph);
+   //  For a sphere at the origin, the position
+   //  and normal vectors are the same
+   glNormal3d(x,y,z);
+   glVertex3d(x,y,z);
+}
+
+static void ball(double x,double y,double z,double r)
+{
+   int th,ph;
+   float yellow[] = {1.0,1.0,0.0,1.0};
+   float Emission[]  = {0.0,0.0,0.01*emission,1.0};
+   //  Save transformation
+   glPushMatrix();
+   //  Offset, scale and rotate
+   glTranslated(x,y,z);
+   glScaled(r,r,r);
+   //  White ball
+   glColor3f(1,1,1);
+   glMaterialf(GL_FRONT,GL_SHININESS,shiny);
+   glMaterialfv(GL_FRONT,GL_SPECULAR,yellow);
+   glMaterialfv(GL_FRONT,GL_EMISSION,Emission);
+   //  Bands of latitude
+   for (ph=-90;ph<90;ph+=inc)
+   {
+      glBegin(GL_QUAD_STRIP);
+      for (th=0;th<=360;th+=2*inc)
+      {
+         Vertex(th,ph);
+         Vertex(th,ph+inc);
+      }
+      glEnd();
+   }
+   //  Undo transofrmations
+   glPopMatrix();
+}
+
+void idle()
+{
+   //  Elapsed time in seconds
+   double t = glutGet(GLUT_ELAPSED_TIME)/1000.0;
+   zh = fmod(90*t,360.0);
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
 }
 
 void key(unsigned char ch,int x,int y)
@@ -108,10 +143,10 @@ void key(unsigned char ch,int x,int y)
 	   if(house6 == 0){ house6 = 1;}
 	   else{ house6 = 0;}
    }
-   else if (ch == 'a' && dim > 1){
+   else if (ch == 'z' && dim > 1){
    		dim -= 0.1;
    }
-   else if (ch == 'f' ){
+   else if (ch == 'Z' ){
    		dim += 0.1;
    }
    else if (ch == 'm' || ch == 'M'){
@@ -123,13 +158,59 @@ void key(unsigned char ch,int x,int y)
    else if (ch == '+' && ch<179){
    		fov++;
    }
+   else if (ch == 'l' || ch == 'L'){
+      light = 1-light;
+   }
+   else if (ch=='['){
+      ylight -= 0.1;
+    }
+   else if (ch==']'){
+      ylight += 0.1;
+    }
+   else if (ch=='a' && ambient>0){
+      ambient -= 5;
+    }
+   else if (ch=='A' && ambient<100){
+      ambient += 5;
+    }
+    else if (ch=='d' && diffuse>0){
+      diffuse -= 5;
+   }
+   else if (ch=='D' && diffuse<100){
+      diffuse += 5;
+    }
+   else if (ch=='s' && specular>0){
+      specular -= 5;
+    }
+   else if (ch=='S' && specular<100){
+      specular += 5;
+    }
+   else if (ch=='e' && emission>0){
+      emission -= 5;
+    }
+   else if (ch=='E' && emission<100){
+      emission += 5;
+    }
+   else if (ch=='n' && shininess>-1){
+      shininess -= 1;
+    }
+   else if (ch=='N' && shininess<7){
+      shininess += 1;
+    }
+   else if (ch == 'r' || ch == 'R'){
+      move = 1-move;
+    }
    else if (ch == 27){
 		exit(0);
    }
-  	Project();
+   shiny = shininess<0 ? 0 : pow(2.0,shininess);
+   Project(mode?fov:0,asp,dim);
+   glutIdleFunc(move?idle:NULL);
    //  Redisplay
    glutPostRedisplay();
 }
+
+
 
 void Floor(){					//Builds House floor
 	glBegin(GL_QUADS);
@@ -223,6 +304,37 @@ void display()
       glRotatef(th,0,1,0);
    }
   
+  if (light){
+        //  Translate intensity to color vectors
+        float Ambient[]   = {0.01*ambient ,0.01*ambient ,0.01*ambient ,1.0};
+        float Diffuse[]   = {0.01*diffuse ,0.01*diffuse ,0.01*diffuse ,1.0};
+        float Specular[]  = {0.01*specular,0.01*specular,0.01*specular,1.0};
+        //  Light position
+        float Position[]  = {distance*Cos(zh),ylight,distance*Sin(zh),1.0};
+        //  Draw light position as ball (still no lighting here)
+        glColor3f(1,1,1);
+        ball(Position[0],Position[1],Position[2] , 0.1);
+        //  OpenGL should normalize normal vectors
+        glEnable(GL_NORMALIZE);
+        //  Enable lighting
+        glEnable(GL_LIGHTING);
+        //  Location of viewer for specular calculations
+        glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER,local);
+        //  glColor sets ambient and diffuse color materials
+        glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
+        glEnable(GL_COLOR_MATERIAL);
+        //  Enable light 0
+        glEnable(GL_LIGHT0);
+        //  Set ambient, diffuse, specular components and position of light 0
+        glLightfv(GL_LIGHT0,GL_AMBIENT ,Ambient);
+        glLightfv(GL_LIGHT0,GL_DIFFUSE ,Diffuse);
+        glLightfv(GL_LIGHT0,GL_SPECULAR,Specular);
+        glLightfv(GL_LIGHT0,GL_POSITION,Position);
+   }
+   else{
+    glDisable(GL_LIGHTING);
+   }
+
   if(house1 == 1){		// If statements determine whether a house is displayed or not
 	house();
   }
@@ -258,7 +370,16 @@ void display()
   }  
   
   glWindowPos2i(5,5);
-  Print("Angle=%d,%d  Dim=%.1f FOV=%d Projection=%s",th,ph,dim,fov,mode?"Perpective":"Orthogonal");
+  Print("Angle=%d,%d  Dim=%.1f FOV=%d Projection=%s Light=%s",
+     th,ph,dim,fov,mode?"Perpective":"Orthogonal",light?"On":"Off");
+   if (light)
+   {
+      glWindowPos2i(5,45);
+      Print("Model=%s LocalViewer=%s Distance=%d Elevation=%.1f",smooth?"Smooth":"Flat",local?"On":"Off",distance,ylight);
+      glWindowPos2i(5,25);
+      Print("Ambient=%d  Diffuse=%d Specular=%d Emission=%d Shininess=%.0f",ambient,diffuse,specular,emission,shiny);
+   }
+
   glFlush();
   glutSwapBuffers();    
 }
@@ -285,7 +406,7 @@ void special(int key,int x,int y)
    th %= 360;
    ph %= 360;
 
-   Project();
+   Project(mode?fov:0,asp,dim);
    //  Tell GLUT it is necessary to redisplay the scene
    glutPostRedisplay();
 }
@@ -294,29 +415,12 @@ void special(int key,int x,int y)
  *  GLUT calls this routine when the window is resized <---- Also taken from ex6.c
  */
 
-void reshape(int width,int height) 
-{
-   // //  Ratio of the width to the height of the window
-   // double w2h = (height>0) ? (double)width/height : 1;
-   // //  Set the viewport to the entire window
-   // glViewport(0,0, width,height);
-   // //  Tell OpenGL we want to manipulate the projection matrix
-   // glMatrixMode(GL_PROJECTION);
-   // //  Undo previous transformations
-   // glLoadIdentity();
-   // //  Orthogonal projection box adjusted for the
-   // //  aspect ratio of the window
-   // glOrtho(-dim*w2h,+dim*w2h, -dim,+dim, -dim,+dim);
-   // //  Switch to manipulating the model matrix
-   // glMatrixMode(GL_MODELVIEW);
-   // //  Undo previous transformations
-   // glLoadIdentity();
-
+void reshape(int width,int height) {
 	asp = (height>0) ? (double)width/height : 1;
    //  Set the viewport to the entire window
    glViewport(0,0, width,height);
    //  Set projection
-   Project();	
+   Project(mode?fov:0,asp,dim);	
 }
 
 /*
@@ -340,7 +444,9 @@ int main(int argc,char* argv[])
    glutSpecialFunc(special);
    //  Tell GLUT to call "key" when a key is pressed
    glutKeyboardFunc(key);
+   glutIdleFunc(idle);
    //  Pass control to GLUT so it can interact with the user
+   ErrCheck("init");
    glutMainLoop();
    //  Return code
    return 0;
